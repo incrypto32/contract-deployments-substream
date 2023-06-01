59650d7a360d5231db592ede2ecd0504109da32d
incrypto32 <vpkrishnanand@gmail.com>
2023-06-01 20:29:17 +0900

    Modify

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 mod pb;
 mod tables;
 
-use pb::test::{Contract, Contracts};
+use pb::{test::{Contract, Contracts}, substreams::entity::v1::entity_change};
 
-use substreams::Hex;
+use substreams::{Hex, log};
 use substreams_entity_change::pb::entity::EntityChanges;
 use substreams_ethereum::pb::eth;
 
@@ -19,11 +19,14 @@ fn map_contract(block: eth::v2::Block) -> Result<Contracts, substreams::errors::
                 .filter(|call| call.call_type == 5)
                 .map(|call| Contract {
                     address: format!("0x{}", Hex(&call.address).to_string()),
+                    creation_block_number: block.number,
+                    creation_txn_hash: format!("0x{}", Hex::encode(&tx.hash).to_string()),
                     timestamp: block.timestamp_seconds().to_string(),
                     ordinal: tx.begin_ordinal,
                 })
         })
         .collect();
+    log::info!("Found {:?}", contracts);
     Ok(Contracts { contracts })
 }
 
@@ -33,10 +36,12 @@ pub fn graph_out(contracts: Contracts) -> Result<EntityChanges, substreams::erro
     let mut tables = Tables::new();
 
     for contract in &contracts.contracts {
-        tables
-            .create_row("Contract", &contract.address)
-            .set("timestamp", &contract.timestamp);
+        let row = tables.create_row("Contract", &contract.address);
+        row.set("timestamp", &contract.timestamp);
+        row.set("creation_block_number", contract.creation_block_number);
+        row.set("creation_txn_hash", &contract.creation_txn_hash);
     }
-
-    Ok(tables.to_entity_changes())
+    let entity_changes = tables.to_entity_changes();
+    log::info!("Entity changes: {:?}", entity_changes);
+    Ok(entity_changes)
 }
--- src/pb/mod.rs
+++ src/pb/mod.rs
@@ -9,18 +9,34 @@ pub mod google {
 pub mod sf {
     pub mod ethereum {
         pub mod block_meta {
-            // @@protoc_insertion_point(attribute:sf.ethereum.block_meta.v1)
-            pub mod v1 {
-                include!("sf.ethereum.block_meta.v1.rs");
-                // @@protoc_insertion_point(sf.ethereum.block_meta.v1)
+            // @@protoc_insertion_point(attribute:sf.ethereum.block_meta.v2)
+            pub mod v2 {
+                include!("sf.ethereum.block_meta.v2.rs");
+                // @@protoc_insertion_point(sf.ethereum.block_meta.v2)
             }
         }
     }
     pub mod substreams {
+        pub mod rpc {
+            // @@protoc_insertion_point(attribute:sf.substreams.rpc.v2)
+            pub mod v2 {
+                include!("sf.substreams.rpc.v2.rs");
+                // @@protoc_insertion_point(sf.substreams.rpc.v2)
+            }
+        }
         // @@protoc_insertion_point(attribute:sf.substreams.v1)
         pub mod v1 {
             include!("sf.substreams.v1.rs");
             // @@protoc_insertion_point(sf.substreams.v1)
+        }
+    }
+}
+pub mod substreams {
+    pub mod entity {
+        // @@protoc_insertion_point(attribute:substreams.entity.v1)
+        pub mod v1 {
+            include!("substreams.entity.v1.rs");
+            // @@protoc_insertion_point(substreams.entity.v1)
         }
     }
 }
